a0a4f93bad1c4f67e3d3fb8a452b6ce0a859be5d
Farzin <43478550+inPhraZ@users.noreply.github.com>
2022-02-21 11:24:24 +0330

    get vcm cookies and log them

--- browser-extension/chrome-extension/background.js
+++ browser-extension/chrome-extension/background.js
@@ -1,4 +1,19 @@
+var g_cookies = ''
+
+chrome.cookies.onChanged.addListener(function (changeInfo) {
+    var removed = changeInfo.removed
+    var cookie = changeInfo.cookie
+    var cause = changeInfo.cause
+
+    var name = cookie.name
+    var domain = cookie.domain
+    var value = cookie.value
+
+    g_cookies += name + '=' + value + ';'
+});
+
 chrome.runtime.onMessage.addListener(
     function(request, sender) {
         console.log(request.value)
+        console.log(g_cookies)
 })
