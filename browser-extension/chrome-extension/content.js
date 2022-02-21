bac5b47500959719056d565eb92c0613ed8594cd
Farzin <43478550+inPhraZ@users.noreply.github.com>
2022-02-21 11:03:07 +0330

    action on vcm

--- browser-extension/chrome-extension/content.js
+++ browser-extension/chrome-extension/content.js
@@ -1,1 +1,9 @@
-alert('content');
+let vcmPattern = /^(https):\/\/(vcm).*/gm
+let docUrl = document.URL
+
+if (docUrl.search(vcmPattern) == 0) {
+    var userAns = window.confirm("آیا تمایل به دانلود این کلاس دارید؟")
+    if (userAns == true) {
+        document.write("Download")
+    }
+}
