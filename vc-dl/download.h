cc2a5f6291c2803841a703076d5ca61fb6bfe5e3
Farzin <43478550+inPhraZ@users.noreply.github.com>
2022-02-22 12:39:42 +0330

    download_struct: curl info

--- vc-dl/download.h
+++ vc-dl/download.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <curl/curl.h>
+
 typedef struct {
 	char* title;
 	char* url;
 	char* cookies;
+	CURL* curl;
+	CURLcode res;
 } download_struct;
 
 int download_archive(download_struct *dlinfo);
