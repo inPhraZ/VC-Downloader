525b1370c23225e079d530e27f79aee5186a4dcd
Farzin <monseffarzin@gmail.com>
2022-02-24 17:08:16 +0330

    two seperate procs: caller: the main proc that extension talks to. callee: download and probably merge the files together

--- vc-dl/nativeMessaging.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define     MAX_MSG_LEN     (1024*1024)
-
-// send msg to extension
-unsigned int sendToExtension(const char* msg);
-
-// recieve msg from extension
-char* recieveFromExtension(unsigned int* len);
