cfb78506ac8c60b97e87d9a1b848182f36e3449a
Farzin <monseffarzin@gmail.com>
2022-02-27 00:37:38 +0330

    IDC_STATIC: info about target

--- callee/vcdl-callee/Resource.h
+++ callee/vcdl-callee/Resource.h
@@ -11,6 +11,7 @@
 #define IDI_SMALL                       108
 #define IDR_MAINFRAME                   128
 #define IDC_PROGRESS1                   1000
+#define IDC_PERCENT                     1001
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -20,7 +21,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        129
 #define _APS_NEXT_COMMAND_VALUE         32773
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
