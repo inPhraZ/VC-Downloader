f86df91f0bfc394219a90d1ed6ce4ef12343ce7c
Farzin <monseffarzin@gmail.com>
2022-02-26 19:14:56 +0330

    new resource: progress dialog

--- callee/vcdl-callee/Resource.h
+++ callee/vcdl-callee/Resource.h
@@ -6,9 +6,11 @@
 #define IDD_VCDLCALLEE_DIALOG           102
 #define IDS_APP_TITLE                   103
 #define IDM_ABOUT                       104
+#define IDD_DOWNLOAD_PROGRESS           106
 #define IDI_VCDLCALLEE                  107
 #define IDI_SMALL                       108
 #define IDR_MAINFRAME                   128
+#define IDC_PROGRESS1                   1000
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -18,7 +20,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        129
 #define _APS_NEXT_COMMAND_VALUE         32773
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
